56c2fc7426a752b9cf72f64eb5d06d5749db46d1
= <=>
2022-12-11 01:06:12 +0000

    commit fix unknown

--- OnlineShop web/Pages/DisplayProductsBase.cs
+++ OnlineShop web/Pages/DisplayProductsBase.cs
@@ -1,6 +1,11 @@
-﻿namespace Mark.Up.Hub.web.Pages
+﻿using Microsoft.AspNetCore.Components;
+using ShopOnline.Models.DTOs;
+
+namespace Mark.Up.Hub.web.Pages
 {
     public class DisplayProductsBase
     {
+        [Parameter]
+        public IEnumerable<ProductDto> Productss { get; set; }
     }
 }
--- OnlineShop web/Pages/ProductsBase.cs
+++ OnlineShop web/Pages/ProductsBase.cs
@@ -13,5 +13,16 @@ namespace Mark.Up.Hub.web.Pages
         {
             Products = await ProductServices.GetProducts();
         }
+        protected IOrderedEnumerable<IGrouping<int, ProductDto>> GetGroupedProductsByCategory()
+        {
+            return from product in Products
+                   group product by product.CategoryId into prodGroupByCatGroup
+                   orderby prodGroupByCatGroup.Key
+                   select prodGroupByCatGroup;
+        }
+        protected string GetCategoryName(IGrouping<int, ProductDto> GroupedPDtoroduct) {
+            return GroupedPDtoroduct.FirstOrDefault(pg => pg.CategoryId == GroupedPDtoroduct.Key).CategoryName;
+
+        }
     }
 }
